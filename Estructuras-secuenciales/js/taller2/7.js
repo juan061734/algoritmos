60d8a7a649db5ae5e45fa1763481e5122cb921fb
jfelipeq14 <juan_fquintero@soy.sena.edu.co>
2023-10-20 20:37:12 -0500

    ejercicio 7 - taller 2

--- Estructuras-secuenciales/js/taller2/7.js
+++ Estructuras-secuenciales/js/taller2/7.js
@@ -1,1 +1,9 @@
 // La presión, el volumen y la temperatura de una masa de aire se relacionan por la fórmula: Masa = (presión * volumen)/(0.37 * (temperatura + 460))
+
+const presion = parseFloat(prompt("Ingrese la presión"))
+const volumen = parseFloat(prompt("Ingrese el volumen"))
+const temperatura = parseFloat(prompt("Ingrese la temperatura"))
+
+const masa = (presion * volumen) / (0.37 * (temperatura + 460))
+
+alert(`La masa de aire es ${masa}`)
